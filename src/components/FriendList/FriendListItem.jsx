98ac9f51800091ce2392f470cb0ad1f14fb19e2e
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-05-04 23:43:27 +0300

    Friend list

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import defaultimg from './default.png'
+import css from './FriendList.module.css'
+
+export function FriendListItem({ status, avatar, name }) {
+  if (!avatar) {
+    avatar = defaultimg;
+  }
+  return (
+    <li className={css.item}>
+      <span className={status? css.online : css.offline}></span>
+      <img className={css.avatar} src={avatar??defaultimg} alt="User avatar" width="48" />
+      <p className={status? css.name : css.noname}>{name}</p>
+</li>
+  )
+}
